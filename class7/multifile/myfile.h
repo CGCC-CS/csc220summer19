87362ce04a8a7f412128613068a29be5530f2957
Wade Huber <wadehuber@gmail.com>
2019-06-19 00:39:27 -0400

    Class 7 examples

---
+++ class7/multifile/myfile.h
@@ -0,0 +1,13 @@
+#ifndef MYFILE_H  /* Check if myfile.h has already been included */
+#define MYFILE_H  /* If it hasn't been defined, define it */
+
+/* Symbolic constants */
+#define FAVORITE 42 
+
+/* Forward Declarations */
+void init(void);
+int addone(int n);
+void doubleIt(int *n);
+int factorial(int n);
+
+#endif
